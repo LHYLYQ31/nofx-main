1fdba46423fd0fdc207dddb7e7ef5864622559d6
ljx@snaple.tech <ljx@snaple.tech>
2026-03-30 10:24:35 +0800

    chore: remove cached artifacts from dev and ignore temp caches

--- .tmp-go-build/.gomodcache/github.com/sonirico/vago@v0.10.0/ptr/ptr.go
+++
@@ -1,8 +0,0 @@
-// Package ptr provides pointer utility functions for Go.
-package ptr
-
-// Ptr returns a pointer to the given value.
-// This is useful for getting pointers to literals or values that need to be passed as pointers.
-func Ptr[T any](x T) *T {
-	return &x
-}
